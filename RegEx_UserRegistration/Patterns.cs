e3d7526ad499aa743d3f965ead92171ca68688d4
Pankaj-Thorat <thoratpankaj282@gmail.com>
2023-05-20 02:23:45 +0530

    [Pankaj] To validate the First Name of User.

---
+++ RegEx_UserRegistration/Patterns.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegEx_UserRegistration
+{
+    class Patterns
+    {
+        public static string REGEX_Name = "^[A-Z][a-zA-Z]{2,}";
+        public bool validateName(string name)
+        {
+            return Regex.IsMatch(name, REGEX_Name);
+        }
+    }
+}
---
+++ RegEx_UserRegistration/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegEx_UserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Patterns user = new Patterns();
+            Console.WriteLine(user.validateName("Pa"));
+        }
+    }
+}
