91fada42772928d8418f64466b7885ff36ad41a6
Pankaj-Thorat <thoratpankaj282@gmail.com>
2023-05-27 23:19:51 +0530

    [Pankaj] To validate password for minimum 8 characters.

--- RegEx_UserRegistration/Patterns.cs
+++ RegEx_UserRegistration/Patterns.cs
@@ -9,9 +9,10 @@ namespace RegEx_UserRegistration
 {
     class Patterns
     {
-        public static string REGEX_NAME = "^[A-Z][a-zA-Z]{2,}";
+        public static string REGEX_NAME  = "^[A-Z][a-zA-Z]{2,}";
         public static string REGEX_EMAIL = @"^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+\.(?:[a-zA-Z]{2,}(?:\.[a-zA-Z]{2})?)$";
         public static string REGEX_PHONE = @"^(?:(?:\+|0{0,2})91(\s*[\ -]\s*)?|[0]?)?[789]\d{9}$";
+        public static string REGEX_PASS  = @"^.{8,}$";
         public bool validateFirstName(string firstname)
         {
             return Regex.IsMatch(firstname, REGEX_NAME);
@@ -29,6 +30,10 @@ namespace RegEx_UserRegistration
         public bool validatePhone(string phone)
         {
             return Regex.IsMatch(phone, REGEX_PHONE);
+        }
+        public bool validatePass(string pass)
+        {
+            return Regex.IsMatch(pass, REGEX_PASS);
         }
     }
 }
--- RegEx_UserRegistration/Program.cs
+++ RegEx_UserRegistration/Program.cs
@@ -13,22 +13,31 @@ namespace RegEx_UserRegistration
             Patterns user = new Patterns();
             //Examples
             //for first name
+            Console.WriteLine("For First Name");
             Console.WriteLine(user.validateFirstName("Pankaj"));//True
             Console.WriteLine(user.validateFirstName("pankaj"));//False
             Console.WriteLine(user.validateFirstName("pa"));//False
             //for last name
+            Console.WriteLine("For Last Name");
             Console.WriteLine(user.validateLastName("Th"));//False
             Console.WriteLine(user.validateLastName("thorat"));//False
             Console.WriteLine(user.validateLastName("Thorat"));//True
             //for email
+            Console.WriteLine("For Email");
             Console.WriteLine(user.validateEmail("abc@bl.co"));//True
             Console.WriteLine(user.validateEmail("abc.xyz@bl.co.in"));//True
             Console.WriteLine(user.validateEmail("Thorat+100@.pl.com"));///False
             //for phone number
+            Console.WriteLine("For Phone No.");
             Console.WriteLine(user.validatePhone("9890854276"));   //True
             Console.WriteLine(user.validatePhone("91 9890854276"));//True
             Console.WriteLine(user.validatePhone("+919890854276"));//True
             Console.WriteLine(user.validatePhone("+91989085427"));//False (9 digit)
+            //for password
+            Console.WriteLine("For Password");
+            Console.WriteLine(user.validatePass("pankaj123"));//True
+            Console.WriteLine(user.validatePass("pankaj@12310"));//True
+            Console.WriteLine(user.validatePass("pankaj"));//False
 
         }
     }
